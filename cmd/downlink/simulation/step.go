880a39ea63a95b822a5e688f6196cb97bc583d0f
cmathieu <kws_guru@yahoo.com>
2021-06-07 23:31:25 -0700

    added EventInfoMapping data structure

--- cmd/downlink/simulation/step.go
+++ cmd/downlink/simulation/step.go
@@ -150,6 +150,94 @@ func (r *VEHICLE) timeStep(i int32) { // i = stage
 //	fmt.Println("gamma = ", rad2deg(r.Stages[i].gamma))
 }
 
+
+func (r *VEHICLE) timeStepSAVE(i int32) { // i = stage
+
+	dgamma := float64(0.0)
+	if r.SysGuidance._MEI1 {
+		dm = float64(r.Stages[i].RunningEngines) * r.Stages[i].ThrottleRate * EnginesMap[r.Stages[i].EngineID].Flow_rate * r.Stages[i].dt
+		r.Stages[i].Mf = r.Stages[i].Mf - dm;
+		r.Stages[i].Mass = r.Stages[i].Mass - dm;
+	}
+
+	if r.SysGuidance._release {
+		aerodynPressure = 0.5 * rho(r.Stages[i].DTF - Re) * r.Stages[i].VRelative * r.Stages[i].VRelative * 1e-3				// Aerodynamic stress
+		drag = (0.5) * r.Stages[i].Cd * r.Stages[i].CSArea * rho(r.Stages[i].DTF - Re) * r.Stages[i].VRelative * r.Stages[i].VRelative		// Drag
+		r.Stages[i].Thrust = float64(r.Stages[i].RunningEngines) * r.Stages[i].ThrottleRate * r.GetThrust(r.Stages[i].DTF, i)	// Thrust
+
+		// calculate force and velocity vectors norm 
+		gh := g(r.Stages[i].DTF)
+		r.Stages[i].Force = (r.Stages[i].Thrust  - drag) - r.Stages[i].Mass * gh * math.Sin(r.Stages[i].gamma)
+		r.Stages[i].RVel = r.Stages[i].RVel + (r.Stages[i].Force/r.Stages[i].Mass) * r.Stages[i].dt
+		r.Stages[i].AVel = r.Stages[i].RVel + vE
+		
+		// calculate altitude: dh/dt = v.sin(gamma)
+		r.Stages[i].altitude = r.Stages[i].RVel * math.Sin(r.Stages[i].gamma) * r.Stages[i].dt + r.Stages[i].altitude
+
+		// calculate downrange dx/dt = (Re/(Re+h))*v*cos(gamma)
+		r.Stages[i].drange = (Re/(Re + r.Stages[i].altitude)) * r.Stages[i].RVel * math.Cos(r.Stages[i].gamma) * r.Stages[i].dt + r.Stages[i].drange
+
+		// calculate dgamma/dt = -(g - v*v/(Re+h)) * cos(gamma) * 1/v
+		//dgamma = -(gh - (r.Stages[i].Vel * r.Stages[i].Vel)/(Re + r.Stages[i].altitude)) * math.Cos(r.Stages[i].gamma) * (1/r.Stages[i].Vel)
+		// dgamma = gamma /()
+//		dgamma = (profile.OrbitPerigee - r.Stages[i].altitude)*1e-3 / (profile.BurnoutTime - r.Stages[i].Clock)
+
+		//dgamma = r.gravTurnClassic(i)
+
+		dgamma = r.gravTurn(i)
+
+		// calculate cartesian coordinates
+		r.Stages[i].alpha = r.Stages[i].drange / Re // polar angle (in rd) based on downrange value
+		r.Stages[i].cx = (Re + r.Stages[i].altitude) * math.Sin(r.Stages[i].alpha)
+		r.Stages[i].cy = (Re + r.Stages[i].altitude) * math.Cos(r.Stages[i].alpha)
+
+
+
+		// x-direction	
+//		r.Stages[i].ForceX = r.Stages[i].Thrust * math.Cos(r.Stages[i].gamma) - drag * math.Cos(r.Stages[i].alpha) - r.Stages[i].Mass * g(r.Stages[i].DTF) * math.Cos(r.Stages[i].beta)
+//		r.Stages[i].ForceX = r.Stages[i].Force * math.Cos(r.Stages[i].gamma)
+//		r.Stages[i].cx = r.Stages[i].cx + r.Stages[i].vAx * r.Stages[i].dt
+		r.Stages[i].ax = r.Stages[i].Force * math.Cos(r.Stages[i].gamma) / r.Stages[i].Mass
+
+		r.Stages[i].vRx = r.Stages[i].vRx + r.Stages[i].ax * r.Stages[i].dt
+		r.Stages[i].vAx = r.Stages[i].vRx + vE //* math.Sin(r.Stages[i].beta)
+
+		// y-direction	
+//		r.Stages[i].ForceY = r.Stages[i].Thrust * math.Sin(r.Stages[i].gamma) - drag * math.Sin(r.Stages[i].alpha) - r.Stages[i].Mass * g(r.Stages[i].DTF) * math.Sin(r.Stages[i].beta)
+//		r.Stages[i].cy = r.Stages[i].cy + r.Stages[i].vAy * r.Stages[i].dt
+		r.Stages[i].ay = r.Stages[i].Force * math.Sin(r.Stages[i].gamma) / r.Stages[i].Mass
+
+		r.Stages[i].vAy = r.Stages[i].vAy + r.Stages[i].ay * r.Stages[i].dt
+		r.Stages[i].vRy = r.Stages[i].vAy //- vE * math.Cos(M_PI + r.Stages[i].beta)
+
+		r.Stages[i].DTF = math.Sqrt(r.Stages[i].cx * r.Stages[i].cx + r.Stages[i].cy * r.Stages[i].cy)
+		r.Stages[i].VAbsolute = math.Sqrt(r.Stages[i].vAx * r.Stages[i].vAx + r.Stages[i].vAy * r.Stages[i].vAy)
+		r.Stages[i].VRelative = math.Sqrt(r.Stages[i].vRx * r.Stages[i].vRx + r.Stages[i].vRy * r.Stages[i].vRy)
+		r.Stages[i].Acc = r.Stages[i].Force / r.Stages[i].Mass //math.Sqrt(r.Stages[i].ax * r.Stages[i].ax + r.Stages[i].ay * r.Stages[i].ay)
+	}
+/*
+	if r.SysGuidance._BBURN  || r.SysGuidance._LBURN {
+		r.flip(0)
+	}
+	if r.SysGuidance._LBURN && mod(r.Stages[i].Clock, 5) < r.Stages[i].dt {
+		r.update_landing_throttle()
+	}
+*/
+
+/*
+	r.Stages[i].cx = r.Stages[i].cx + r.Stages[i].vAx * r.Stages[i].dt
+	r.Stages[i].cy = r.Stages[i].cy + r.Stages[i].vAy * r.Stages[i].dt
+*/
+	if r.Stages[i].gamma >= math.Abs(dgamma) {
+		r.Stages[i].gamma = r.Stages[i].gamma - math.Abs(dgamma)
+	} else {
+		r.Stages[i].gamma = 0
+	}
+
+//	fmt.Println("gamma = ", rad2deg(r.Stages[i].gamma))
+}
+
+
 var nearOrbit = false
 var gammabits float64
 
--- packages/data/events.go
+++ packages/data/events.go
@@ -20,24 +20,30 @@ const (
 	E_THROTTLE_U = 0x00010000 // throttle engine down
 )
 
-var EventMapping = map[string]uint32{
-	"MEI":                E_MEI_1,
-	"LIFT_OFF":           E_LIFTOFF,
-	"PITCH":              E_STARTPITCH,
-	"MECO":               E_MECO_1,
-	"STAGE_SEP":          E_STAGESEP,
-	"SEI":                E_SEI_1,
-	"BOOSTBACK_BURN_ON":  E_BBURNI,
-	"BOOSTBACK_BURN_OFF": E_BBURNO,
-	"ENTRY_BURN_ON":      E_EBURNI,
-	"ENTRY_BURN_OFF":     E_EBURNO,
-	"SECO":               E_SECO_1,
-	"SEI2":               E_SEI_2,
-	"SECO2":              E_SECO_2,
-	"LANDING_BURN_ON":    E_LBURNI,
-	"LANDING_BURN_OFF":   E_LBURNO,
-	"THROTTLE_DWN":       E_THROTTLE_D,
-	"THROTTLE_UP":        E_THROTTLE_U,
+type eventInfo struct {
+	id uint32
+	label string
+
+}
+
+var EventInfoMapping = map[string]eventInfo {
+	"MEI":                {E_MEI_1,  		"Main engines ignition"},
+	"LIFT_OFF":           {E_LIFTOFF, 		"Lift off"},
+	"PITCH":              {E_STARTPITCH, 	"Pitch starts"},
+	"MECO":               {E_MECO_1, 		"Main engine cut off"},
+	"STAGE_SEP":          {E_STAGESEP,		"Stage Separation"},
+	"SEI":                {E_SEI_1,			"Second stage ignition"},
+	"BOOSTBACK_BURN_ON":  {E_BBURNI,		"Boostback burn started"},
+	"BOOSTBACK_BURN_OFF": {E_BBURNO,		"Boostback burn stopped"},
+	"ENTRY_BURN_ON":      {E_EBURNI,		"Entry burn started"},
+	"ENTRY_BURN_OFF":     {E_EBURNO,		"Entry burn stopped"},
+	"SECO":               {E_SECO_1,		"Second stage engine cut off"},
+	"SEI2":               {E_SEI_2,			"Second stage 2nd engine ignition"},
+	"SECO2":              {E_SECO_2,		"Second stage 2nd engine cut off"},
+	"LANDING_BURN_ON":    {E_LBURNI,		"Landing burn started"},
+	"LANDING_BURN_OFF":   {E_LBURNO,		"Landing burn stopped"},
+	"THROTTLE_DWN":       {E_THROTTLE_D,	"Throttling down"},
+	"THROTTLE_UP":        {E_THROTTLE_U,	"Throttling up"},
 }
 
 var EventMappingString = map[string]string{
@@ -59,3 +65,24 @@ var EventMappingString = map[string]string{
 	"THROTTLE_DWN":       "Throttling down",
 	"THROTTLE_UP":        "Throttling up",
 }
+
+var EventMapping = map[string]uint32{
+	"MEI":                E_MEI_1,
+	"LIFT_OFF":           E_LIFTOFF,
+	"PITCH":              E_STARTPITCH,
+	"MECO":               E_MECO_1,
+	"STAGE_SEP":          E_STAGESEP,
+	"SEI":                E_SEI_1,
+	"BOOSTBACK_BURN_ON":  E_BBURNI,
+	"BOOSTBACK_BURN_OFF": E_BBURNO,
+	"ENTRY_BURN_ON":      E_EBURNI,
+	"ENTRY_BURN_OFF":     E_EBURNO,
+	"SECO":               E_SECO_1,
+	"SEI2":               E_SEI_2,
+	"SECO2":              E_SECO_2,
+	"LANDING_BURN_ON":    E_LBURNI,
+	"LANDING_BURN_OFF":   E_LBURNO,
+	"THROTTLE_DWN":       E_THROTTLE_D,
+	"THROTTLE_UP":        E_THROTTLE_U,
+}
+
